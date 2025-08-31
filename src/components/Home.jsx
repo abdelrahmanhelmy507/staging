02e242619eb106b93c07587aa19d1f2dcd53f64c
Abdelrahman Helmy <helmyabdelrahman335@gmail.com>
2025-08-31 18:51:35 +0300

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+
+import Header from "./Header";
+import Hero from "./Hero/Hero";
+import Services from "./services";
+import Pricing from "./Pricing";
+import Features from "./Features";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Solution from "./solution";
+
+export default function Home() {
+
+    return (
+        <>
+            
+            <Hero />
+            <Services />
+            <Pricing />
+            <CTA />
+            <Solution />
+            <Features />
+            
+        </>
+    )
+}  
